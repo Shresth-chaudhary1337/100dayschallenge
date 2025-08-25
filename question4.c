531cc3824c1341ae3e6578ec02ad75d24ec9872c
Shresth-chaudhary1337 <chaudharyshresth07@gmail.com>
2025-08-26 01:21:19 +0530

    Update question4.c

--- question4.c
+++ question4.c
@@ -1,3 +1,4 @@
+//Q4: Write a program to calculate the area and circumference of a circle given its radius.
 #include<stdio.h>
 
 int main()
@@ -8,3 +9,4 @@ int main()
     printf("area of circle is %lf and circumference is %lf", 3.14*r*r,2*3.14*r);
     return 0;
 }
+
